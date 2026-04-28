976d19957e257d92343cc718b83ac8f277c8ba8b
Roly <takewhatsyoursroly@gmail.com>
2026-04-28 09:38:51 -0400

    feat(shared): add alembic with initial migration

---
+++ packages/shared/alembic/env.py
@@ -0,0 +1,25 @@
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config, pool
+from alembic import context
+from liveintent_shared.config import get_settings
+from liveintent_shared.models import Base
+
+config = context.config
+if config.config_file_name:
+    fileConfig(config.config_file_name)
+
+config.set_main_option("sqlalchemy.url", get_settings().database_url)
+target_metadata = Base.metadata
+
+def run_migrations_online():
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+        with context.begin_transaction():
+            context.run_migrations()
+
+run_migrations_online()
